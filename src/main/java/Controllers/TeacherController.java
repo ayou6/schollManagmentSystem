ddbafdff0d54a51c07f0fdd2f626f50745bb0ed8
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 03:51:11 -0700

    editing class teacherController

--- src/main/java/Controllers/TeacherController.java
+++ src/main/java/Controllers/TeacherController.java
@@ -1,5 +1,15 @@
 package Controllers;
 
+import Model.Teacher;
+import Services.TeacherServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
 public class TeacherController {
+
+
 
 }
