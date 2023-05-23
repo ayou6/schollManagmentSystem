749fd6e13dd9022059cb2eb3269ebdc92ad56bbd
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 04:12:26 -0700

    add student done

--- src/main/java/Controllers/StudentController.java
+++ src/main/java/Controllers/StudentController.java
@@ -22,5 +22,10 @@ public class StudentController {
         return studentServices.getById(id);
     }
 
+    @PostMapping("/addStudent")
+    public Student addStudent (@RequestBody Student student){
+        return studentServices.addStudent(student);
+    }
+
 }
 
