02fa0918b716cf73699e1315974ae4893ed67cd7
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 05:15:11 -0700

    editing the code

--- src/main/java/Repository/ClassRepository.java
+++ src/main/java/Repository/ClassRepository.java
@@ -1,9 +1,10 @@
 package Repository;
 
+import Model.ClassRoom;
 import Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClassRepository extends JpaRepository<Class, Long> {
+public interface ClassRepository extends JpaRepository<ClassRoom, Long> {
 }
