34862f5267912be65821eccc3d17b559ebdd9e5a
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 04:33:14 -0700

    ClassController (fixing issue)

--- src/main/java/Controllers/ClassController.java
+++ src/main/java/Controllers/ClassController.java
@@ -2,9 +2,7 @@ package Controllers;
 
 import Services.ClassServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +15,7 @@ public class ClassController {
     public List<Class> getAllClasses(){
         return classServices.getAllClasses();
     }
+
 
 
 }
