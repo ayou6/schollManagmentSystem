4cbbda252855a4d7a602629991446221e9e8f709
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 04:10:53 -0700

    getby id for StudentController is done

--- src/main/java/Controllers/StudentController.java
+++ src/main/java/Controllers/StudentController.java
@@ -3,9 +3,7 @@ package Controllers;
 import Model.Student;
 import Services.StudentServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class StudentController {
     public List<Student> getAllStudents(){
         return studentServices.getAllStudents();
     }
+    @GetMapping("{id}")
+    public Student getById(@PathVariable ("id") Long id){
+        return studentServices.getById(id);
+    }
+
 }
 
