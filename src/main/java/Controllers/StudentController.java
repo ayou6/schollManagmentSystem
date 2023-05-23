ab6c9a231232b09e474a813e0841cfe0ec2b06db
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 04:06:33 -0700

    Completing StudentController

--- src/main/java/Controllers/StudentController.java
+++ src/main/java/Controllers/StudentController.java
@@ -1,4 +1,17 @@
 package Controllers;
 
+import Model.Student;
+import Services.StudentServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "student")
 public class StudentController {
+
 }
+
