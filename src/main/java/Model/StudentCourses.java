9c6521bddd838e741332267076c13e9bbc691684
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 05:30:59 -0700

    completing StudentCourses

--- src/main/java/Model/StudentCourses.java
+++ src/main/java/Model/StudentCourses.java
@@ -2,12 +2,17 @@ package Model;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Setter
 @Getter
 @Entity
 public class StudentCourses {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private int grades;
 
