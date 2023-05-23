a629628e0da4e914db775b428dbf9740291aba9e
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 03:50:49 -0700

    editing class CourseController

--- src/main/java/Controllers/CourseController.java
+++ src/main/java/Controllers/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "Course")
+@RequestMapping(value = "course")
 public class CourseController {
     @Autowired
     CourseServices courseServices;
