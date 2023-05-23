a50245f509e10697f5603f355e9c2c17dd6c28ea
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-22 23:41:13 -0700

    newproject

---
+++ src/main/java/Repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package Repository;
+
+import Model.Course;
+import Model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
