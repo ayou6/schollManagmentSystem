3295a67db35c9c29c1a9eda9f192a3d05e9e8f18
Your Username goes here <ayoxb.abbas@gmail.com>
2023-05-23 04:31:09 -0700

    ClassController (adding autowierd and mapping )

--- src/main/java/Controllers/ClassController.java
+++ src/main/java/Controllers/ClassController.java
@@ -1,5 +1,18 @@
 package Controllers;
 
+import Services.ClassServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "class")
 public class ClassController {
+    @Autowired
+    ClassServices classServices;
+
 
 }
