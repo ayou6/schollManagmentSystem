e35395f38cfc283272878b989600618f4b170fb9
ayou6 <ayoxb.abbas@gmail.com>
2023-05-25 16:47:11 +0400

    adding attirbutes

---
+++ src/main/java/com/example/schoolManagementSystem/Requests/AddCourseRequest.java
@@ -0,0 +1,21 @@
+package com.example.schoolManagementSystem.Requests;
+
+import com.example.schoolManagementSystem.Model.Course;
+import com.example.schoolManagementSystem.Model.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddCourseRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private String code;
+    private boolean isActive;
+
+}
