524e2688a12912428279683074e2f14dc56c61ee
ayou6 <ayoxb.abbas@gmail.com>
2023-05-25 10:56:19 +0400

    updating the class Course

--- src/main/java/com/example/schoolManagementSystem/Model/Course.java
+++ src/main/java/com/example/schoolManagementSystem/Model/Course.java
@@ -14,6 +14,7 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String Description;
     @OneToOne
     Teacher teacher;
     @ManyToOne
