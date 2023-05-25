f6c1efe9f62741d85ae39f9c514afc318a465660
ayou6 <ayoxb.abbas@gmail.com>
2023-05-25 16:46:41 +0400

    add Course request

--- src/main/java/com/example/schoolManagementSystem/Model/Course.java
+++ src/main/java/com/example/schoolManagementSystem/Model/Course.java
@@ -1,10 +1,10 @@
 package com.example.schoolManagementSystem.Model;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+        import lombok.Data;
+        import lombok.Getter;
+        import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
+        import javax.persistence.*;
+        import java.util.List;
 @Data
 @Setter
 @Getter
